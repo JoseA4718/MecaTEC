326a8bc93834e413fe96c24d2ac04c3ff21b5956
Jose <joseantonioestudiante2001@gmail.com>
2022-09-19 22:05:12 -0600

    Updated Mobile project
    Added login page and register page

---
+++ MecaTEC_App/MecaTEC_App/MecaTEC_App/Views/Login_Page.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MecaTEC_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    /// <summary>
+    /// This class allows users to login in Cook Time application, if already have an account created.
+    /// @author Jose A.
+    /// </summary>
+    public partial class Login_Page : ContentPage
+
+    {
+        public static string ip = "192.168.0.100";
+        /// <summary>
+        /// This constructor execute Login Page partial class
+        /// @author Jose A.
+        /// </summary>
+        public Login_Page()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// This method is used to change the current page to Register page
+        /// @author Mauricio C.
+        /// </summary>
+        private void Go_Register(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Views.Register_Page());
+        }
+
+        /// <summary>
+        /// This method is used to change the current page to Home page and send a json file to verify the user credentials
+        /// @author Mauricio C.
+        /// </summary>
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            /*
+            var userValidate = userEntry.Text;
+            if (!string.IsNullOrEmpty(userValidate))
+            {
+
+                string email = userEntry.Text;
+                string password = "/" + CreateMD5(pasEntry.Text);
+                HttpClient cliente = new HttpClient();
+                string url = "http://" + ip + ":6969/login/" + email + password;
+                var result = await cliente.GetAsync(url);
+                var json = result.Content.ReadAsStringAsync().Result;
+                CookTime.REST_API_UserModel.User InputUser = new REST_API_UserModel.User();
+                InputUser = CookTime.REST_API_UserModel.User.FromJson(json);
+                if (InputUser.Name == null)
+                {
+                    await DisplayAlert("Cook Time", "The data you filled with does not match with any CookTime user. Please verify your info and try again!", "OK");
+                }
+                else
+                {
+                    CURRENTUSER = CookTime.REST_API_UserModel.User.FromJson(json);
+                    await DisplayAlert("Cook Time", "Welcome back " + CURRENTUSER.Name, "OK");
+                    await Navigation.PushAsync(new HomePage());
+                }
+            }*/
+
+        }
+        /// <summary>
+        /// This method simply updates the user information, everytime a change has been made to itself.
+        /// @author Jose A.
+        /// </summary>
+        public static async void updateUser()
+        {
+            /*
+            HttpClient cliente = new HttpClient();
+            string url = "http://" + ip + ":6969/login/" + CURRENTUSER.Email + "/" + CURRENTUSER.Password;
+            var result = await cliente.GetAsync(url);
+            var json = result.Content.ReadAsStringAsync().Result;
+            CookTime.REST_API_UserModel.User InputUser = new REST_API_UserModel.User();
+            InputUser = CookTime.REST_API_UserModel.User.FromJson(json);
+            if (InputUser.Name == null)
+            {
+                return;
+            }
+            else
+            {
+                CURRENTUSER = CookTime.REST_API_UserModel.User.FromJson(json);
+
+            }*/
+        }
+        /// <summary>
+        /// This method is used to change the current page to Info page
+        /// @author Mauricio C.
+        /// </summary>
+        private void Info_Clicked(object sender, EventArgs e)
+        {/*
+            Navigation.PushAsync(new AboutPage());*/
+        }
+        /// <summary>
+        /// This method encrypt the password with HASH MD5 algorithm
+        /// @author Mauricio C.
+        /// </summary>
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ MecaTEC_App/MecaTEC_App/MecaTEC_App/Views/Register_Page.xaml.cs
@@ -0,0 +1,118 @@
+﻿//using CookTime.REST_API_Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MecaTEC_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    /// <summary>
+    /// This class allows users to register in Cook Time application
+    /// @author Jose A.
+    /// </summary>
+    public partial class Register_Page : ContentPage
+    {
+        /// <summary>
+        /// This constructor execute Register Page partial class and send data of the new user
+        /// @author Jose A.
+        /// </summary>
+        public Register_Page()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// This method create an async request that send data of the new user
+        /// @author Jose A.
+        /// </summary>
+        private async void RegisterRequest()
+        {
+            /*
+            Newuser user = new Newuser();
+            user.Age = Int32.Parse(ageEntry.Text);
+            user.Email = emailEntry.Text;
+            user.Password = CreateMD5(passwordEntry.Text);
+            user.Name = nameEntry.Text;
+            HttpClient cliente = new HttpClient();
+            if (ChefBox.IsChecked)
+            {
+                string url = "http://" + LoginPage.ip + ":6969/newChef";//AQUI DEBE IR EL URL PARA CHEFS 
+                String jsonNewUser = JsonConvert.SerializeObject(user);
+                Console.WriteLine("JSON NEW USER:" + jsonNewUser);
+                var datasent = new StringContent(jsonNewUser);
+                Console.WriteLine("DATASENT" + datasent);
+                datasent.Headers.ContentType.MediaType = "application/json";
+                var result = await cliente.PostAsync(url, datasent);
+                var json = result.Content.ReadAsStringAsync().Result;
+                await DisplayAlert("Result", json, "ok");
+            }
+            else
+            {
+                string url = "http://" + LoginPage.ip + ":6969/newUser";//AQUI DEBE IR EL URL PARA USUARIOS SINGULARES
+                String jsonNewUser = JsonConvert.SerializeObject(user);
+                Console.WriteLine("JSON NEW USER:" + jsonNewUser);
+                var datasent = new StringContent(jsonNewUser);
+                Console.WriteLine("DATASENT" + datasent);
+                datasent.Headers.ContentType.MediaType = "application/json";
+                var result = await cliente.PostAsync(url, datasent);
+                var json = result.Content.ReadAsStringAsync().Result;
+                await DisplayAlert("Result", json, "ok");
+            }*/
+
+
+        }
+
+
+        /// <summary>
+        /// This method verify that not exist blank spaces and send the final request
+        /// @author Mauricio C.
+        /// </summary>
+        private void Button_Clicked(object sender, EventArgs e)
+        {/*
+            var nameValidate = nameEntry.Text;
+            var ageValidate = ageEntry.Text;
+            var emailValidate = emailEntry.Text;
+            var passValidate = passwordEntry.Text;
+            if (!string.IsNullOrEmpty(nameValidate) && !string.IsNullOrEmpty(ageValidate) && !string.IsNullOrEmpty(emailValidate) && !string.IsNullOrEmpty(passValidate))
+            {
+                RegisterRequest();
+                DisplayAlert("REGISTRATION COMPLETE", "ENJOY COOKTIME!", "ACCEPT");
+                Navigation.PushAsync(new LoginPage());
+            }
+            else
+            {
+                DisplayAlert("ERROR", "YOU MUST FILL ALL THE BLANKS TO CONTINUE", "ACCEPT");
+            }*/
+        }
+
+
+        /// <summary>
+        /// This method encrypt the password with HASH MD5 algorithm
+        /// @author Jose A.
+        /// </summary>
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
