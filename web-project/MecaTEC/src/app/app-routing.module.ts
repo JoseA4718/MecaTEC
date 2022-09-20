9ec08df6d4133ecc95724f54c371fd8ec30c854c
Juan Rodriguez <juanrm071299@gmail.com>
2022-09-20 01:54:52 -0600

    login and routes web app

--- web-project/MecaTEC/src/app/app-routing.module.ts
+++ web-project/MecaTEC/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {LoginComponent} from './views/login/login.component'
+import {DashboardComponent} from './views/dashboard/dashboard.component'
+import {EditarComponent} from './views/editar/editar.component'
+import {NuevoComponent} from './views/nuevo/nuevo.component'
+const routes: Routes = [
+  {path:'',redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'nuevo', component:NuevoComponent},
+  {path: 'editar', component:EditarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponets = [LoginComponent,DashboardComponent,EditarComponent,NuevoComponent]
